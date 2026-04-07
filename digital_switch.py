2ff4ec361334cb688104f9c2881b9d4ceb648dd3
Zinawe <kostkost2007@yandex.ru>
2026-04-07 16:02:22 +0300

    10 task

--- digital_switch.py
+++ digital_switch.py
@@ -10,9 +10,17 @@ GPIO.setup(button, GPIO.IN)
 GPIO.setup(led, GPIO.OUT)
 
 state = 0
+flag = False
 
 while True:
-    if GPIO.input(button):
+    cur_state = GPIO.input(button)
+    
+    if cur_state and not flag:
         state = not state
         GPIO.output(led, state)
-        time.sleep(0.2)
+        flag = True
+    
+    elif not cur_state and flag:
+        flag = False
+    
+    time.sleep(0.02)
